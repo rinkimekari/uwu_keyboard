d2dbed8c95b2d2e31b095ae36b717cbdf67e92f7
Micah Wasmuth <micahwasmuth@gmail.com>
2022-07-12 20:28:35 -0400

    begin uwu code

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,14 @@
 use rdev::{
     listen, simulate, Event, EventType,
     Key::{
-        Alt, AltGr, Backspace, ControlLeft, ControlRight, KeyL, KeyR, KeyW,
-        MetaLeft, MetaRight, ShiftLeft,
+        Alt, AltGr, Backspace, ControlLeft, ControlRight, KeyL, KeyR, KeyU,
+        KeyW, MetaLeft, MetaRight, ShiftLeft,
     },
 };
 
 static mut NO_MODS: u8 = 0;
+static mut LAST: char = ' ';
+static mut LAST_LAST: char = ' ';
 
 fn main() {
     listen(callback).unwrap();
@@ -23,6 +25,11 @@ fn callback(event: Event) {
                     replace(&event.name.as_ref().unwrap());
                 }
             }
+            KeyU => {
+                if unsafe { NO_MODS == 0 } {
+                    uwu();
+                }
+            }
             _ => {}
         },
         EventType::KeyRelease(key) => match key {
@@ -33,6 +40,21 @@ fn callback(event: Event) {
         },
         _ => {}
     }
+
+    unsafe {
+        LAST_LAST = LAST;
+    }
+
+    // gets first char of Option<String>
+    unsafe {
+        LAST = event
+            .name
+            .map(|s| s.chars().next())
+            .flatten()
+            .unwrap_or(LAST);
+    }
+
+    println!("\n LAST = {}\n", unsafe { LAST });
 }
 
 // get rid of this function if delay isnt needed for MacOS
@@ -52,9 +74,18 @@ fn replace(c: &String) {
         send(&EventType::KeyPress(ShiftLeft));
         send(&EventType::KeyPress(KeyW));
         send(&EventType::KeyRelease(KeyW));
-        // send(&EventType::KeyRelease(ShiftLeft)); // just trust me bro
+        // send(&EventType::KeyRelease(ShiftLeft)); // can't send cuz reasons (just think about it)
     } else if c == "r" || c == "l" {
         send(&EventType::KeyPress(KeyW));
         send(&EventType::KeyRelease(KeyW));
+    }
+}
+
+fn uwu() {
+    if unsafe { LAST_LAST } != 'w' {
+        send(&EventType::KeyPress(KeyW));
+        send(&EventType::KeyRelease(KeyW));
+        send(&EventType::KeyPress(KeyU));
+        send(&EventType::KeyRelease(KeyU));
     }
 }
